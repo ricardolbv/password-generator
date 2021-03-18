8cdf3a1aae7ed2af56b35ab8856b544d6304b487
Ricardo Vieira <ricardo.biscola.vieira@gmail.com>
2021-03-17 22:24:46 -0300

    Creating app bar

---
+++ src/common/TopBar.js
@@ -0,0 +1,29 @@
+import AppBar  from '@material-ui/core/AppBar';
+import ToolBar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography'
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles({
+    barText: {
+      marginLeft: 'auto',
+      marginRight: 'auto',
+    },
+});
+  
+
+
+const TopBar = () => {
+    const classes = useStyles();
+
+    return (
+    <AppBar>
+        <ToolBar>
+            <Typography variant='h5' className={classes.barText} >
+                Password generator
+            </Typography>
+        </ToolBar>
+    </AppBar>
+    )
+}
+
+export default TopBar;
